a7af394386d5c8b6bad019dce11d120eadde1c3b
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-03-09 16:52:16 +0100

    greenSock

---
+++ public/js/main.js
@@ -0,0 +1,122 @@
+// animation nav
+TweenMax.from(".header", 2,{
+    delay: 1,
+    y:"-60",
+    ease:Expo.easeInOut,
+
+})
+TweenMax.from(".una", 3,{
+    delay :1.5,
+    x:-50,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".unc", 3,{
+    delay :1.5,
+    x:+50,
+    ease:Expo.easeInOut,
+})
+
+
+TweenMax.from(".deuxa", 3,{
+    delay :3.5,
+    x:-250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".deuxaimg", 3,{
+    delay :4.5,
+    opacity: 0,
+    x:+250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".deuxb", 3,{
+    delay :3.5,
+    x:+250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".deuxbimg", 3,{
+    delay :4.5,
+    opacity: 0,
+    x:+250,
+    ease:Expo.easeInOut,
+})
+
+
+
+
+TweenMax.from(".imgtroisa", 3,{
+    delay :4.5,
+    opacity: 0,
+    x:-150,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".imgtroisb", 3,{
+    delay :5.5,
+    opacity: 0,
+    x:-200,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".imgtroisc", 3,{
+    delay :6.5,
+    opacity: 0,
+    x:-250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".imgtroisd", 3,{
+    delay :7.5,
+    opacity: 0,
+    x:-250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".imgtroise", 3,{
+    delay :8.5,
+    opacity: 0,
+    x:-250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".imgtroisf", 3,{
+    delay :9.5,
+    opacity: 0,
+    x:-250,
+    ease:Expo.easeInOut,
+})
+
+TweenMax.from(".testimonialcarda", 3,{
+    delay :10.5,
+    opacity: 0,
+    x:+250,
+    ease:Expo.easeInOut,
+})
+// TweenMax.from(".testimonialcardb", 3,{
+//     delay :10.5,
+//     opacity: 0,
+//     x:+250,
+//     ease:Expo.easeInOut,
+// })
+TweenMax.from(".testimonialcardc", 3,{
+    delay :10.5,
+    opacity: 0,
+    x:-250,
+    ease:Expo.easeInOut,
+})
+
+
+
+TweenMax.from(".bloga", 3,{
+    delay :11,
+    opacity: 0,
+    x:+250,
+    ease:Expo.easeInOut,
+})
+
+TweenMax.from(".blogb", 3,{
+    delay :11,
+    opacity: 0,
+    y:+250,
+    ease:Expo.easeInOut,
+})
+TweenMax.from(".blogc", 3,{
+    delay :11,
+    opacity: 0,
+    y:+250,
+    ease:Expo.easeInOut,
+})``
